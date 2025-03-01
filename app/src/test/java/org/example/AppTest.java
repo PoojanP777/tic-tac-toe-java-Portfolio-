d16d4b91816f3224f3b6596f61433389be18bd33
PoojanP777 <patelpoojan664@gmail.com>
2025-03-01 13:03:10 -0600

    tic tac toe
    Output of tic tac toe

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,9 +4,57 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  @Test
-  void appHasAGreeting() {
-    App classUnderTest = new App();
-    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-  }
+
+    @Test
+    void testInitialBoardState() {
+        App game = new App();
+        char[] expectedBoard = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        assertArrayEquals(expectedBoard, game.getBoard());
+    }
+
+    @Test
+    void testValidMove() {
+        App game = new App();
+        assertTrue(game.makeMove(1));
+    }
+
+    @Test
+    void testInvalidMoveOutOfBounds() {
+        App game = new App();
+        assertFalse(game.makeMove(10));
+    }
+
+    @Test
+    void testInvalidMoveAlreadyTaken() {
+        App game = new App();
+        game.makeMove(1);
+        assertFalse(game.makeMove(1));
+    }
+
+    @Test
+    void testWinCondition() {
+        App game = new App();
+        game.makeMove(1);
+        game.makeMove(4);
+        game.makeMove(2);
+        game.makeMove(5);
+        game.makeMove(3);
+        assertTrue(game.checkWin());
+    }
+
+    @Test
+    void testDrawCondition() {
+        App game = new App();
+        game.makeMove(1);
+        game.makeMove(2);
+        game.makeMove(3);
+        game.makeMove(5);
+        game.makeMove(4);
+        game.makeMove(6);
+        game.makeMove(8);
+        game.makeMove(7);
+        game.makeMove(9);
+        assertTrue(game.isDraw());
+    }
 }
+
